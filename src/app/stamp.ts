ea229712598de8e8dcb42e92f05bbc092596a42c
ultimatecookie <loic.vierin@gmail.com>
2019-11-05 18:58:15 +0100

    stamp class

---
+++ src/app/stamp.ts
@@ -0,0 +1,10 @@
+import { Color } from '@ionic/core';
+
+export class Stamp {
+    country: string;
+    year: number;
+    color: Color;
+    fiscal: boolean;
+    postal: boolean;
+    picture: string;
+}
