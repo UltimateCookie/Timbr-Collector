222fde5deea08ccd3e64b87029df87325af4fbcb
ultimatecookie <loic.vierin@gmail.com>
2019-11-06 15:51:16 +0100

    subject added

--- src/app/home/stamps/stamps.component.ts
+++ src/app/home/stamps/stamps.component.ts
@@ -7,25 +7,27 @@ import { ModalStampDetailsPage } from '../modal-stamp-details/modal-stamp-detail
 @Component({
   selector: 'app-stamps',
   templateUrl: './stamps.component.html',
-  styleUrls: ['./stamps.component.scss'],
+  styleUrls: ['./stamps.component.scss']
 })
 export class StampsComponent implements OnInit {
     stamps: Stamp[];
+    favFilter: boolean;
 
   constructor(public navCtrl: NavController, private stampService: StampService, public modalController: ModalController) { }
 
   ngOnInit() {
-    this.getStamps();
+    this.stampService.stampsSubject.subscribe( (stamps: any[]) => {this.stamps = stamps;});
+    this.stampService.emitStampsSubject();
   }
 
   goToNewStampPage(): void {
     this.navCtrl.navigateForward("new-stamp");
   }
 
-  getStamps(): void {
-    this.stampService.getStamps()
-        .subscribe(stamps => this.stamps = stamps);
-  }
+  // getStamps(): void {
+  //   this.stampService.getStamps()
+  //       .subscribe(stamps => {this.stamps = stamps;});
+  // }
 
   async presentModal(stamp: Stamp) {
     const modal = await this.modalController.create({
@@ -36,5 +38,11 @@ export class StampsComponent implements OnInit {
     });
     return await modal.present();
   }
+
+  checkFav() {
+    this.favFilter = !this.favFilter;
+  }
+
+  
 
 }
--- src/app/stamp.service.ts
+++ src/app/stamp.service.ts
@@ -1,28 +1,31 @@
 import { Injectable } from '@angular/core';
 import { FAKE_STAMPS } from './test-fake-stamps';
 import { Stamp } from './stamp';
-import { Observable, of } from 'rxjs';
+import { Observable, of, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StampService {
   stamps: Stamp[] = FAKE_STAMPS;
+  stampsSubject = new Subject<Stamp[]>();
 
   constructor() { }
 
-  getStamps(): Observable<Stamp[]> {
-    return of(this.stamps);
-  }
 
   addStamp(stamp: Stamp): boolean {
     this.stamps.push(stamp);
+    this.emitStampsSubject();
     return true;
-
   }
 
-  setFavStatus(id: number, fav: boolean): void { //todo: passer le stamp directement.
+  setFavStatus(id: number, fav: boolean): void {
     this.stamps[id-1].fav = fav;
+    this.emitStampsSubject();
+  }
+
+  emitStampsSubject() {
+    this.stampsSubject.next(this.stamps);
   }
 
   //connect to API REST here
